d4e135e357f8fc228c5b45264a3f98db9e39e08d
= <=>
2025-09-17 10:35:23 +0530

    <leader>bK close all but current

--- init.lua
+++ init.lua
@@ -1257,3 +1257,19 @@ end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+
+-- Command to close all buffers except the current one
+vim.api.nvim_create_user_command("BufOnly", function()
+  local current = vim.api.nvim_get_current_buf()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted then
+      if buf ~= current then
+        vim.api.nvim_buf_delete(buf, { force = false })
+      end
+    end
+  end
+end, { desc = "Close all buffers except the current one" })
+
+-- Map <leader>bK to run :BufOnly
+vim.keymap.set("n", "<leader>bK", "<cmd>BufOnly<cr>", { desc = "Keep only current buffer" })
