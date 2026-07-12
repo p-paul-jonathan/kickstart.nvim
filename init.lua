fc3bab26c044a9cb3968aadcf58d8e47fe3d7212
P Paul Jonathan <ppauljonathan@gmail.com>
2026-07-12 19:13:05 +0530

    update 12/07/26

--- init.lua
+++ init.lua
@@ -1059,17 +1059,17 @@ require('lazy').setup({
   },
   {
     'shaunsingh/nord.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd 'colorscheme nord'
+    end,
   },
   {
     'sainnhe/everforest',
   },
   {
     'catppuccin/nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd 'colorscheme catppuccin-mocha'
-    end,
   },
   {
     'barrientosvctor/abyss.nvim',
